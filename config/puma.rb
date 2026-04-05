b64524e85f7d78e4eeb7fbf2b7d4f9a5ef696faf
Miguel A. Reyna D. <sysmarddev@gmail.com>
2026-04-04 20:45:11 -0500

    update puma.rb
    se actualizo puma la configuración para que sea compatible con local y servidor.

--- config/puma.rb
+++ config/puma.rb
@@ -17,19 +17,26 @@
 #bind "unix:///home/batisoftware/crm_2software/tmp/sockets/puma.sock"
 
 # config/puma.rb
-directory '/var/www/crm_2software'
-rackup "/var/www/crm_2software/config.ru"
-environment 'production'
-
-pidfile "/var/www/crm_2software/shared/tmp/pids/puma.pid"
-state_path "/var/www/crm_2software/shared/tmp/pids/puma.state"
-stdout_redirect "/var/www/crm_2software/shared/log/puma.stdout.log", "/var/www/crm_2software/shared/log/puma.stderr.log", true
-
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+threads_count = ENV.fetch("RAILS_MAX_THREADS", 5).to_i
 threads threads_count, threads_count
 
-workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+environment ENV.fetch("RAILS_ENV") { "development" }
 
-bind "tcp://127.0.0.1:3001"
+port ENV.fetch("PORT", 3000)
+
+pidfile ENV.fetch("PIDFILE", "tmp/pids/server.pid")
 
 plugin :tmp_restart
+
+if ENV["RAILS_ENV"] == "production"
+  workers ENV.fetch("WEB_CONCURRENCY", 2).to_i
+
+  app_dir = ENV.fetch("APP_DIR", File.expand_path("..", __dir__))
+  directory app_dir
+
+  stdout_redirect(
+    ENV.fetch("PUMA_STDOUT_LOG", "log/puma.stdout.log"),
+    ENV.fetch("PUMA_STDERR_LOG", "log/puma.stderr.log"),
+    true
+  )
+end
